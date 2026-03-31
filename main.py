48ce3baea31ec7f915eea443a25d323cc8a1626b
gdranka-ux <g.dranka@escola.pr.gov.br>
2026-03-31 07:07:49 -0300

    Update main.py
    Tomara que não bugue isso que só são comentários, deixei metade sem fazer, para o Diego fazer!

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-lista = []
-producao_total = 0
+lista = []              #Uma lista Vazia para armazenar os dados da produção
+producao_total = 0        #As duas variaveis uma de soma e a outra um contador de vezes que a produção caiu
 queda_producao = 0
 
-for i in range (16):
-    producao = int(input("Digite a produção da hora "))
-    lista.append (producao)
+for i in range (16):       #Loop de 16 para as horas
+    producao = int(input("Digite a produção da hora "))   #Aqui ele pede a produção do Usuário
+    lista.append (producao)       #Adiciona o valor que foi colocado acima
 
-    producao_total += producao
+    producao_total += producao    #Faz a Soma na produção total
 
-    media = producao_total / len(lista)
+    media = producao_total / len(lista)    #Calcula a média
 
-    hora_mais_produtiva = lista[0]
+    hora_mais_produtiva = lista[0]    #Esses 2 definem os valores iniciais (primeiro valor da lista)
     hora_menos_produtiva = lista[0]
 
 for i in lista:
